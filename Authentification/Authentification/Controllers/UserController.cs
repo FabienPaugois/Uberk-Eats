3b1fad0435a89d7e12557ae6c1bc6c1fe728a12d
Timothée VARLET <timothee.varlet@viacesi.fr>
2022-06-08 16:42:31 +0200

    API

---
+++ Authentification/Authentification/Controllers/UserController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Authentification.Models;
+using Microsoft.Data.SqlClient;
+using System.Data.Common;
+
+namespace Authentification.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        
+
+        private readonly UberkEatsContext db;
+
+        public UserController()
+        {
+            db = new UberkEatsContext();
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "API is working";
+        }
+
+
+        [HttpPost]
+        [Route("/Authenticate")]
+        public User Authenticate(User user)
+        {
+            User authenticated = db.User.FirstOrDefault(a => a.Mail == user.Mail && a.Password == user.Password);
+            if(authenticated != null)
+            {
+                return authenticated;
+            }
+            else
+            {
+                return user;
+            }
+        }
+
+        [HttpPost]
+        [Route("/Delete")]
+        public string Delete(User user)
+        {
+            User authenticated = db.User.FirstOrDefault(a => a.Mail == user.Mail && a.Password == user.Password);
+            if (authenticated != null)
+            {
+                db.User.Remove(authenticated);
+                db.SaveChanges();
+                return "Deleted";
+            }
+            else
+            {
+                return "Password or mail error";
+            }
+        }
+
+        [HttpPost]
+        [Route("/Update")]
+        public string Update(User user)
+        {
+            User authenticated = db.User.FirstOrDefault(a => a.Mail == user.Mail && a.Password == user.Password);
+            if (authenticated != null)
+            {
+                user.Id = authenticated.Id;
+                db.Entry(authenticated).CurrentValues.SetValues(user);
+                db.SaveChanges();
+                return "Updated";
+            }
+            else
+            {
+                return "Password or mail error";
+            }
+        }
+
+        [HttpPost]
+        [Route("/Create")]
+        public string Create(User user)
+        {
+            User authenticated = db.User.FirstOrDefault(a => a.Mail == user.Mail);
+            if (authenticated != null)
+            {
+                return "Mail address already used";
+            }
+            else
+            {
+                user.Id = 0;
+                db.User.Add(user);
+                db.SaveChanges();
+                return "Account Created";
+            }
+        }
+    }
+}
--- Authentification/Authentification/Models/User.cs
+++ Authentification/Authentification/Models/User.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
-namespace Authentification
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Authentification.Models
 {
-    class User
+    public partial class User
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Mail { get; set; }
