eb760da687c8f61f0a45a4b9a869fff4d62ac6f5
Fabien Paugois <fabien.paugois@viacesi.fr>
2022-06-10 16:50:53 +0200

    Used lint fix

--- UberkEatsClients/src/app/components/menu-pick-page/menu-pick-page.component.spec.ts
+++ UberkEatsClients/src/app/components/menu-pick-page/menu-pick-page.component.spec.ts
@@ -3,23 +3,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MenuPickPageComponent } from './menu-pick-page.component';
 
 describe('MenuPickPageComponent', () => {
-  let component: MenuPickPageComponent;
-  let fixture: ComponentFixture<MenuPickPageComponent>;
+	let component: MenuPickPageComponent;
+	let fixture: ComponentFixture<MenuPickPageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ MenuPickPageComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [ MenuPickPageComponent ]
+		})
+			.compileComponents();
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MenuPickPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(MenuPickPageComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
