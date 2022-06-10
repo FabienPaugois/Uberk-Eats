247a4b092cc39f15915f26e1f19d4e210deb60b0
Lucas <lduleu@studioysinteractive.com>
2022-06-10 15:57:55 +0200

    Added Login Register Restaurants & MenusPick pages - WIP

---
+++ UberkEatsClients/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularMaterialModule } from './angular-material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { RegisterPageComponent } from './components/register-page/register-page.component';
+import { ProfilePageComponent } from './components/profile-page/profile-page.component';
+import { RestaurantListComponent } from './components/restaurant-list/restaurant-list.component';
+import { MenuPickPageComponent } from './components/menu-pick-page/menu-pick-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    ProfilePageComponent,
+    RestaurantListComponent,
+    MenuPickPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AngularMaterialModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
