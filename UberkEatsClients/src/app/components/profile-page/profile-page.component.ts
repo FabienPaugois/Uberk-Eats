7c4edcf170740c2c37e98046725fdde8b9bf258a
Lucas <lduleu@studioysinteractive.com>
2022-06-10 15:57:55 +0200

    Added Login Register Restaurants & MenusPick pages - WIP

---
+++ UberkEatsClients/src/app/components/profile-page/profile-page.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Clients } from '../../model/clients';
+import { Roles } from '../../model/roles';
+
+@Component({
+  selector: 'app-profile-page',
+  templateUrl: './profile-page.component.html',
+  styleUrls: ['./profile-page.component.scss']
+})
+export class ProfilePageComponent implements OnInit {
+  public roles = Roles
+  constructor() { }
+  userInfo: Clients = {
+    id: '1',
+    phone: '0675994028',
+    name: 'Lucas',
+    surname: 'Duleu',
+    mail: 'lucas.duleu@hotmail.fr',
+    password: '***',
+    role: this.roles.TechnicalService
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ UberkEatsClients/src/app/components/register-page/register-page.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { ClientsApiService } from '../../services/clients-api.service';
+import { Roles } from "../../model/roles";
+
+@Component({
+  selector: 'app-register-page',
+  templateUrl: './register-page.component.html',
+  styleUrls: ['./register-page.component.scss']
+})
+export class RegisterPageComponent implements OnInit {
+  @Input() registerInfo = { mail: '', password: '', phone: '', name: '', surname: '' };
+  constructor(public clientsApi: ClientsApiService, public router: Router) {
+  }
+  roles: any[] = Object.values(Roles).filter(role => role.toString().length > 2);
+  ngOnInit(): void {
+  }
+
+  register(dataclient: any) {
+    this.clientsApi.Register(this.registerInfo).subscribe((data: {}) => {
+      this.router.navigate(['/']);
+    })
+  }
+}
