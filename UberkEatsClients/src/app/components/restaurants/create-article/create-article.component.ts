ea87fb8957ccad2ba26e80292a7bcc06dea01fd6
Lucas <lduleu@studioysinteractive.com>
2022-06-15 17:45:19 +0200

    Fix form input

--- UberkEatsClients/src/app/components/restaurants/create-article/create-article.component.ts
+++ UberkEatsClients/src/app/components/restaurants/create-article/create-article.component.ts
@@ -11,30 +11,28 @@ import { ClientsApiService } from '../../../services/clients-api.service';
   styleUrls: ['./create-article.component.scss']
 })
 export class CreateArticleComponent implements OnInit {
-  @Input() registerInfo: Articles;
+  @Input() articleInfo: Articles = { id: NaN, name:'', description: '', price: 0, image_url:''};
   roles: any[] = Object.values(Roles).filter(role => role.toString().length > 2);
 
   public registerForm: FormGroup; // variable of type FormGroup is created
   constructor(public clientsApi: ClientsApiService, public router: Router, private fb: FormBuilder) {
     // Form element defined below
     this.registerForm = this.fb.group({
-      name: '',
-      description: '',
-      phone: '',
-      password: '',
-      mail: '',
-      role: ''
+      Name: '',
+      Description: '',
+      Price: '',
+      Image_url: '',
     });
   }
   ngOnInit(): void {
   }
 
-  addArticle(dataclient: any) {
-    this.registerInfo.name = this.registerForm.get('Name')?.value;
-    this.registerInfo.description = this.registerForm.get('Description')?.value;
-    this.registerInfo.price = this.registerForm.get('Price')?.value;
-    this.registerInfo.image_url = this.registerForm.get('Image_URL')?.value;
-    this.clientsApi.register(this.registerInfo).subscribe((data: unknown) => {
+  addArticle() {
+    this.articleInfo.name = this.registerForm.get('Name')?.value;
+    this.articleInfo.description = this.registerForm.get('Description')?.value;
+    this.articleInfo.price = this.registerForm.get('Price')?.value;
+    this.articleInfo.image_url = this.registerForm.get('Image_url')?.value;
+    this.clientsApi.register(this.articleInfo).subscribe((data: unknown) => {
       this.router.navigate(['/']);
     });
   }
