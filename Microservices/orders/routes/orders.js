c484f0fa7c386a05288554d67b0115dce10e3659
Timothée VARLET <timothee.varlet@viacesi.fr>
2022-06-21 19:06:41 +0200

    Added orders microservice

---
+++ Microservices/orders/routes/orders.js
@@ -0,0 +1,230 @@
+const express = require("express")
+var router = express.Router({ mergeParams: true });
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+router.use(bodyParser.json());
+const accessTokenSecret = 'a23f5zddoznJFGZBiGbIg895FZK';
+
+var orderSchema = mongoose.Schema({
+	clientId: Number,
+	deliverAdress: String,
+	deliverymanId: Number,
+	restaurantId: Number,
+	status: Number,
+	timestamp: Object,
+	products: Object
+});
+
+var Order = mongoose.model('Orders', orderSchema);
+
+
+const authenticateJWT = (req, res, next) => {
+	const authHeader = req.headers.authorization;
+
+	if (authHeader) {
+		const token = authHeader.split(' ')[1];
+
+		jwt.verify(token, accessTokenSecret, (err, user) => {
+			if (err) {
+				return res.sendStatus(403);
+			}
+
+			req.user = user;
+			next();
+		});
+	} else {
+		res.sendStatus(401);
+	}
+}
+
+//#region GetAllOrders
+
+/**
+ * @api {get} /orders Request all Orders information
+ * @apiName GetOrders
+ * @apiGroup Orders
+ *
+ * @apiSuccess {Number} clientId Id of the User who ordered.
+ * @apiSuccess {String} deliverAdress DeliverAdress of the Order.
+ * @apiSuccess {Number} deliverymanId DeliverymanId of the Order.
+ * @apiSuccess {Number} restaurantId RestaurantId of the Order.
+ * @apiSuccess {Number} status Status of the Order.
+ * @apiSuccess {Object} timestamp Timestamp object of the Order.
+ * @apiSuccess {Object} products Products object of the Order.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *		 "_id": "62b02a1a13dcbfe08b8db704",
+ *		 "clientId": 12,
+ *		 "deliverAdress": "10 Downing Street ",
+ *		 "deliverymanId": 2547,
+ *		 "restaurantId": 147,
+ *		 "status": 2,
+ *		 "timestamp": {
+ *			"createdAt": Number,
+ *			"pickedUpAt": Number,
+ *			"deliveredAt": Number,
+ *			"readyAt": Number
+ *		 },
+ *		 "products": {
+ *			
+ *		 }
+ *     }
+ *      
+ * @apiError OrdersNotAccessible Orders were unaccessible.
+ *
+ */
+/* GET Orders. */
+router.route('/orders')
+
+	.get(authenticateJWT, function (req, res, next) {
+		Order.find(function (err, orders) {
+			if (err) {
+				res.send(err);
+			}
+			res.json(orders);
+		})
+	})
+//#endregion
+//#region PostOrder
+/**
+* @api {post} /order/ Create Order Information
+* @apiVersion 1.0.0
+* @apiName PostOrder
+* @apiGroup Orders
+*
+* @apiSuccess {Number} clientId Id of the User who ordered.
+* @apiSuccess {String} deliverAdress DeliverAdress of the Order.
+* @apiSuccess {Number} deliverymanId DeliverymanId of the Order.
+* @apiSuccess {Number} restaurantId RestaurantId of the Order.
+* @apiSuccess {Number} status Status of the Order.
+* @apiSuccess {Object} timestamp Timestamp object of the Order.
+* @apiSuccess {Object} products Products object of the Order.
+*
+* @apiError OrderNotCreated Order couldn't be created.
+*/
+
+/* POST Orders. */
+router.route('/order').post(authenticateJWT, function (req, res, next) {
+	// Nous utilisons le schéma Order
+	var order = new Order();
+	// Nous récupérons les données reçues pour les ajouter à l'objet Order
+	order.name = req.body.name;
+	order.price = req.body.price;
+	order.description = req.body.description;
+	order.imgUrl = req.body.imgUrl;
+	order.articles = req.body.articles;
+	//Nous stockons l'objet en base
+	order.save(function (err) {
+		if (err) {
+			res.send(err);
+		}
+		else {
+			res.send("Created");
+		}
+	});
+});
+//#endregion
+//#region GetOrdersById
+/**
+ * @api {get} /order/:order_ids Request Orders information
+ * @apiName GetOrder
+ * @apiGroup Order
+ *
+ * @apiParam {string} order_ids List of Order unique IDs, separated by commas.
+ *
+ * @apiSuccess {Number} clientId Id of the User who ordered.
+ * @apiSuccess {String} deliverAdress DeliverAdress of the Order.
+ * @apiSuccess {Number} deliverymanId DeliverymanId of the Order.
+ * @apiSuccess {Number} restaurantId RestaurantId of the Order.
+ * @apiSuccess {Number} status Status of the Order.
+ * @apiSuccess {Object} timestamp Timestamp object of the Order.
+ * @apiSuccess {Object} products Products object of the Order.
+ *
+ * @apiError OrderNotFound The id of one or more Orders were not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "Orders were not found"
+ *     }
+ */
+
+router.route('/order/:order_ids')
+	.get(authenticateJWT, function (req, res) {
+		const ids = req.params.order_ids.split(',');
+		Order.find().where('_id').in(ids).exec((err, orders) => {
+			if (err)
+				res.status(404).json({ message: "Orders were not found" });
+			else
+				res.json(orders);
+		});
+	})
+//#endregion
+//#region PutOrderById
+/**
+* @api {put} /order/:order_id Update Order Information
+* @apiVersion 1.0.0
+* @apiName PutOrders
+* @apiGroup Orders
+*
+* @apiSuccess {String} name Name of the Order.
+* @apiSuccess {String} price Price of the Order.
+* @apiSuccess {String} description Description of the Order.
+* @apiSuccess {String} imgUrl ImgUrl of the Order.
+* @apiSuccess {Array} articles Articles array of the Order
+*
+* @apiError OrderNotUpdated Order couldn't be updated.
+*/
+
+router.route('/order/:order_id').put(authenticateJWT, function (req, res) {
+	Order.findById(req.params.order_id, function (err, order) {
+		if (err) {
+			res.send(err);
+		}
+		order.name = req.body.name;
+		order.price = req.body.price;
+		order.description = req.body.description;
+		order.imgUrl = req.body.imgUrl;
+		order.articles = req.body.articles;
+		order.save(function (err) {
+			if (err)
+				res.status(404).json({ message: "Order couldn't be updated" });
+			else
+				res.json({ message: 'Updated order' });
+		});
+	});
+})
+	//#endregion
+	//#region DeleteOrderById
+	/**
+	* @api {delete} /order/:order_id Delete Order Information
+	* @apiVersion 1.0.0
+	* @apiName DeleteOrders
+	* @apiGroup Orders
+	*
+	* @apiSuccess {Number} clientId Id of the User who ordered.
+	* @apiSuccess {String} deliverAdress DeliverAdress of the Order.
+	* @apiSuccess {Number} deliverymanId DeliverymanId of the Order.
+	* @apiSuccess {Number} restaurantId RestaurantId of the Order.
+	* @apiSuccess {Number} status Status of the Order.
+	* @apiSuccess {Object} timestamp Timestamp object of the Order.
+	* @apiSuccess {Object} products Products object of the Order.
+	*
+	* @apiError OrderNotDeleted Order couldn't be deleted.
+	*/
+	.delete(authenticateJWT, function (req, res) {
+
+		Order.remove({ _id: req.params.order_id }, function (err, order) {
+			if (err) {
+				res.status(404).json({ message: "Order couldn't be deleted" });
+			}
+			else {
+				res.json({ message: "Deleted Order" });
+			}
+		});
+	});
+//#endregion
+module.exports = router;
