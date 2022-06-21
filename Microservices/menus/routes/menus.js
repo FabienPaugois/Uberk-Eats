236fc22c417171937e1f983bf063d37748c9251d
Fabien Paugois <fabien.paugois@viacesi.fr>
2022-06-21 11:45:43 +0200

    Fixed linter issues

--- Microservices/menus/routes/menus.js
+++ Microservices/menus/routes/menus.js
@@ -1,10 +1,10 @@
-var express = require("express")
+const express = require("express")
 var router = express.Router({ mergeParams: true });
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 router.use(bodyParser.json());
-var accessTokenSecret = 'a23f5zddoznJFGZBiGbIg895FZK';
+const accessTokenSecret = 'a23f5zddoznJFGZBiGbIg895FZK';
 
 var menuSchema = mongoose.Schema({
 	name: String,
@@ -17,13 +17,13 @@ var menuSchema = mongoose.Schema({
 var Menu = mongoose.model('Menus', menuSchema);
 
 
-var authenticateJWT = function (req, res, next) {
-	var authHeader = req.headers.authorization;
+const authenticateJWT = (req, res, next) => {
+	const authHeader = req.headers.authorization;
 
 	if (authHeader) {
-		var token = authHeader.split(' ')[1];
+		const token = authHeader.split(' ')[1];
 
-		jwt.verify(token, accessTokenSecret, function (err, user) {
+		jwt.verify(token, accessTokenSecret, (err, user) => {
 			if (err) {
 				return res.sendStatus(403);
 			}
@@ -79,7 +79,6 @@ var authenticateJWT = function (req, res, next) {
  * @apiError MenusNotAccessible Menus were unaccessible.
  *
  */
-
 /* GET Menus. */
 router.route('/menus')
 
@@ -92,7 +91,6 @@ router.route('/menus')
 		})
 	})
 //#endregion
-
 //#region PostMenu
 /**
 * @api {post} /menu/ Create Menu Information
@@ -110,25 +108,26 @@ router.route('/menus')
 */
 
 /* POST Menus. */
-// Nous utilisons le schéma Menu
-var menu = new Menu();
-// Nous récupérons les données reçues pour les ajouter à l'objet Menu
-menu.name = req.body.name;
-menu.price = req.body.price;
-menu.description = req.body.description;
-menu.imgUrl = req.body.imgUrl;
-menu.articles = req.body.articles;
-//Nous stockons l'objet en base
-menu.save(function (err) {
-	if (err) {
-		res.send(err);
-	}
-	else {
-		res.send("Created");
-	}
+router.route('/menu').post(authenticateJWT, function (req, res, next) {
+	// Nous utilisons le schÃ©ma Menu
+	var menu = new Menu();
+	// Nous rÃ©cupÃ©rons les donnÃ©es reÃ§ues pour les ajouter Ã  l'objet Menu
+	menu.name = req.body.name;
+	menu.price = req.body.price;
+	menu.description = req.body.description;
+	menu.imgUrl = req.body.imgUrl;
+	menu.articles = req.body.articles;
+	//Nous stockons l'objet en base
+	menu.save(function (err) {
+		if (err) {
+			res.send(err);
+		}
+		else {
+			res.send("Created");
+		}
+	});
 });
 //#endregion
-
 //#region GetMenusById
 /**
  * @api {get} /menu/:menu_ids Request Menus information
@@ -167,15 +166,16 @@ menu.save(function (err) {
  */
 
 router.route('/menu/:menu_ids')
-var ids = req.params.menu_ids.split(',');
-Menu.find().where('_id').in(ids).exec(function(err, menus){
-	if (err)
-		res.status(404).json({ message: "Menus were not found" });
-	else
-		res.json(menus);
-});
+	.get(authenticateJWT, function (req, res) {
+		const ids = req.params.menu_ids.split(',');
+		Menu.find().where('_id').in(ids).exec((err, menus) => {
+			if (err)
+				res.status(404).json({ message: "Menus were not found" });
+			else
+				res.json(menus);
+		});
+	})
 //#endregion
-
 //#region PutMenuById
 /**
 * @api {put} /menu/:menu_id Update Menu Information
@@ -192,48 +192,50 @@ Menu.find().where('_id').in(ids).exec(function(err, menus){
 * @apiError MenuNotUpdated Menu couldn't be updated.
 */
 
-Menu.findById(req.params.menu_id, function (err, menu) {
-	if (err) {
-		res.send(err);
-	}
-	menu.name = req.body.name;
-	menu.price = req.body.price;
-	menu.description = req.body.description;
-	menu.imgUrl = req.body.imgUrl;
-	menu.articles = req.body.articles;
-	menu.save(function (err) {
-		if (err)
-			res.status(404).json({ message: "Menu couldn't be updated" });
-		else
-			res.json({ message: 'Updated menu' });
+router.route('/menu/:menu_id').put(authenticateJWT, function (req, res) {
+	Menu.findById(req.params.menu_id, function (err, menu) {
+		if (err) {
+			res.send(err);
+		}
+		menu.name = req.body.name;
+		menu.price = req.body.price;
+		menu.description = req.body.description;
+		menu.imgUrl = req.body.imgUrl;
+		menu.articles = req.body.articles;
+		menu.save(function (err) {
+			if (err)
+				res.status(404).json({ message: "Menu couldn't be updated" });
+			else
+				res.json({ message: 'Updated menu' });
+		});
 	});
-});
+})
+	//#endregion
+	//#region DeleteMenuById
+	/**
+	* @api {delete} /menu/:menu_id Delete Menu Information
+	* @apiVersion 1.0.0
+	* @apiName DeleteMenus
+	* @apiGroup Menus
+	*
+	* @apiSuccess {String} name Name of the Menu.
+	* @apiSuccess {String} price Price of the Menu.
+	* @apiSuccess {String} description Description of the Menu.
+	* @apiSuccess {String} imgUrl ImgUrl of the Menu.
+	* @apiSuccess {Array} articles Articles array of the Menu
+	*
+	* @apiError MenuNotDeleted Menu couldn't be deleted.
+	*/
+	.delete(authenticateJWT, function (req, res) {
+
+		Menu.remove({ _id: req.params.menu_id }, function (err, menu) {
+			if (err) {
+				res.status(404).json({ message: "Menu couldn't be deleted" });
+			}
+			else {
+				res.json({ message: "Deleted Menu" });
+			}
+		});
+	});
 //#endregion
-
-//#region DeleteMenuById
-/**
-* @api {delete} /menu/:menu_id Delete Menu Information
-* @apiVersion 1.0.0
-* @apiName DeleteMenus
-* @apiGroup Menus
-*
-* @apiSuccess {String} name Name of the Menu.
-* @apiSuccess {String} price Price of the Menu.
-* @apiSuccess {String} description Description of the Menu.
-* @apiSuccess {String} imgUrl ImgUrl of the Menu.
-* @apiSuccess {Array} articles Articles array of the Menu
-*
-* @apiError MenuNotDeleted Menu couldn't be deleted.
-*/
-
-Menu.remove({ _id: req.params.menu_id }, function (err, menu) {
-	if (err) {
-		res.status(404).json({ message: "Menu couldn't be deleted" });
-	}
-	else {
-		res.json({ message: "Deleted Menu" });
-	}
-});
-//#endregion
-
 module.exports = router;
