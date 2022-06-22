51eb6b99dcb19980cf6693660cd4039b5b8f10ba
Timothée VARLET <timothee.varlet@viacesi.fr>
2022-06-22 16:42:11 +0200

    Je reviens

--- UberkEatsClients/src/app/model/clients.ts
+++ UberkEatsClients/src/app/model/clients.ts
@@ -2,9 +2,9 @@ import { Roles } from './roles';
 
 export interface Clients {
   id: string;
-  phone: string;
-  name: string;
-  surname: string;
+  phone: string | null;
+  name: string | null;
+  surname: string | null;
   mail: string;
   password: string;
   role: Roles;
