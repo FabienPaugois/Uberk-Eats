727db8fd4c91933b0f2b97afa7a8bfff1bf58a83
Timothée VARLET <timothee.varlet@viacesi.fr>
2022-06-22 16:42:11 +0200

    Je reviens

--- UberkEatsClients/src/app/services/clients-api.service.ts
+++ UberkEatsClients/src/app/services/clients-api.service.ts
@@ -41,13 +41,20 @@ export class ClientsApiService {
 	}
 
 	register(employee: any, roleName: any, affiliateMail: any): Observable<AuthToken> {
-		console.log(affiliateMail);
 		return this.http.post<AuthToken>(
 			this.apiURL + '/create',
 			JSON.stringify({ user: employee, roleName, affiliateMail }),
 			this.httpOptions
 		).pipe(retry(0), catchError(this.handleError));
-	}
+  }
+
+  modify(employee: any): Observable<Clients> {
+    return this.http.post<Clients>(
+      this.apiURL + '/update',
+      JSON.stringify({ user: employee }),
+      this.httpOptions
+    ).pipe(retry(0), catchError(this.handleError));
+  }
 
 	getRestaurants(): Observable<Restaurants> {
 		return this.http.get<Restaurants>(
