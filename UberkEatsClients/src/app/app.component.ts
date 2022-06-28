b355b3403d4a88887136d67eef0b14b136269ef1
Timothée VARLET <timothee.varlet@viacesi.fr>
2022-06-28 11:59:55 +0200

    Added logout function

--- UberkEatsClients/src/app/app.component.ts
+++ UberkEatsClients/src/app/app.component.ts
@@ -6,38 +6,43 @@ import { AuthGuard } from './auth.guard';
 import { ClientsApiService } from './services/clients-api.service';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-	title = 'UberkEatsClients';
-	hidden = true;
-	notificationsNumber = 0;
-	constructor(
+  title = 'UberkEatsClients';
+  hidden = true;
+  notificationsNumber = 0;
+  constructor(
     public notificationsApi: NotificationsApiService,
     public router: Router,
-		public clientsApiService: ClientsApiService,
-		public authGuard: AuthGuard
-	) { }
+    public clientsApiService: ClientsApiService,
+    public authGuard: AuthGuard
+  ) { }
 
-	ngOnInit(): void {
-		this.getUserUnreadNotifications();
-		setInterval(() => {
-			this.getUserUnreadNotifications();
-		},5000);
-	}
+  ngOnInit(): void {
+    this.getUserUnreadNotifications();
+    setInterval(() => {
+      this.getUserUnreadNotifications();
+    }, 5000);
+  }
 
-	getUserUnreadNotifications() {
-		this.notificationsApi.getUserUnreadNotifications(JSON.parse(localStorage.getItem('User') as string).Id)
-			.subscribe((notifications: Notifications[]) => {
-				if (notifications.length > 0) {
-					this.hidden = false;
-				}
-				else {
-					this.hidden = true;
-				}
-				this.notificationsNumber = notifications.length;
-			});
-	}
+  getUserUnreadNotifications() {
+    this.notificationsApi.getUserUnreadNotifications(JSON.parse(localStorage.getItem('User') as string).Id)
+      .subscribe((notifications: Notifications[]) => {
+        if (notifications.length > 0) {
+          this.hidden = false;
+        }
+        else {
+          this.hidden = true;
+        }
+        this.notificationsNumber = notifications.length;
+      });
+  }
+
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/login-page']);
+  }
 }
